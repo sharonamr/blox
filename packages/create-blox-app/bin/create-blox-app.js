9a31275c81a5260b04efb0fcf8e18b797e6842d3
Sharon Amram <a.sharon@gmail.com>
2022-12-08 12:14:43 +0200

    Update create-blox-app.js

--- packages/create-blox-app/bin/create-blox-app.js
+++ packages/create-blox-app/bin/create-blox-app.js
@@ -11,6 +11,20 @@ const args = process.argv.slice(2);
 const appName = args[0];
 const SCRIPTS_PATH = path.resolve(__dirname, '../');
 
-console.log('copy files from template to', appName, SCRIPTS_PATH);
+if (!appName) {
+  throw new Error('app name is required. for example "create-blox-app my-app"');
+}
+console.log(`Creating ${appName} blox app`);
 
 copyRecursiveSync(`${SCRIPTS_PATH}/template/`, `${appName}/`);
+
+console.log("Done.\n");
+
+console.log("Now you can:");
+const commands = [
+  `cd ${appName}`,
+  'npm install',
+  'npm start',
+];
+
+console.log(commands.join('\n'));
